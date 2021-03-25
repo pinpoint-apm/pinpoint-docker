37750f5cb320797700276f17275c27a5168510a8
SungWook Kim <sungwook0115.kim@gmail.com>
2021-03-25 18:09:58 +0900

    [#noissue] add version change in flink

--- pinpoint-flink/docker-bin/docker-entrypoint.sh
+++ pinpoint-flink/docker-bin/docker-entrypoint.sh
@@ -3,12 +3,12 @@
 # In flink version < 1.11 we can't use "job cluster" because it's not yet available
 if [ "$1" = "standalone-job" ]; then
   cd /build
-  zip -d pinpoint-flink-job-*.jar "profiles/release/*.properties"
+  zip -d pinpoint-flink-job.jar "profiles/release/*.properties"
   sed -i -e "s/#PINPOINT_ZOOKEEPER_ADDRESS#/${PINPOINT_ZOOKEEPER_ADDRESS}/g" profiles/release/*
-  zip -r pinpoint-flink-job-*.jar profiles/
+  zip -r pinpoint-flink-job.jar profiles/
   cd -
 
-  mv /build/pinpoint-flink-job-*.jar pinpoint-flink-job.jar
+  mv /build/pinpoint-flink-job.jar pinpoint-flink-job.jar
 
   shift
 
