a78e9891cca5580e023771e07aefb542e5c70262
ga-ram <galam.choi@navercorp.com>
2023-04-17 14:07:31 +0900

    Update pinot table names

--- pinpoint-hbase/build/scripts/check-table.sh
+++ pinpoint-hbase/build/scripts/check-table.sh
@@ -3,8 +3,8 @@
 if echo -e "exists 'HostApplicationMap_Ver2'" | ${HBASE_HOME}/bin/hbase shell 2>&1 | grep -q "does exist" 2>/dev/null
 then 
     echo "Tables already exist"
-    sleep 15
-    ${HBASE_HOME}/bin/hbase shell ${BASE_DIR}/hbase-update-ttl.hbase
+    #sleep 15
+    #${HBASE_HOME}/bin/hbase shell ${BASE_DIR}/hbase-update-ttl.hbase
 else
     sleep 15
     echo "create tables"
